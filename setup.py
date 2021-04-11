3955465cf1b7f1bb967c52e73dfe3ec2bb5ad8d6
FaresAhmedb <faresa.bakhit@gmail.com>
2021-04-11 09:43:03 +0200

    Update Version to 0.2

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name                          = 'matrixmanp',
-    version                       = '0.1',
+    version                       = '0.2',
     description                   = 'Matrix Manipulation module to add, substract, multiply matrices.',
     long_description              = long_description,
     long_description_content_type = 'text/markdown',
