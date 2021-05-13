2c7380b9fbb39310280f5c5f6ec4dbfe4b9c16ef
FaresAhmedb <faresa.bakhit@gmail.com>
2021-05-13 19:21:34 +0200

    Fixed `classifiers`

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ setuptools.setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7"
+        "Programming Language :: Python :: 2.7",
         "Topic :: Scientific/Engineering :: Mathematics"
     ],
     )
